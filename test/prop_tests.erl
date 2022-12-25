03bee592b68f0dfa3e10453da44a3c9c8e990ba5
zinovichev_es <ezinovichev@gmail.com>
2022-12-25 20:44:53 +0300

    add associativity test

--- test/prop_tests.erl
+++ test/prop_tests.erl
@@ -17,67 +17,83 @@
   ]
 ).
 
--export([test_properties/1]).
+-export(
+  [
+    prop_add_element/0,
+    prop_remove_element/0,
+    prop_merge_neutral_element/0,
+    prop_merge_associativity_element/0
+  ]
+).
 
--define (TESTS_NUMBER, 100).
+check_hashmap_addition(OldHashmap, NewHashmap, Key) ->
+  (OldHashmap#hashmap.elements_size + 1 == NewHashmap#hashmap.elements_size)
+  and
+  (hashmap_get_value(NewHashmap, Key) /= false).
 
-get_property_test_result(Property) ->
-    proper:quickcheck(Property, [{numtests, ?TESTS_NUMBER}]).
-
-check_hashmap_addition(OldHashmap, NewHashmap, Key) -> 
-    (OldHashmap#hashmap.elements_size + 1 == NewHashmap#hashmap.elements_size)
-    and (hashmap_get_value(NewHashmap, Key) /= false).
-
-prop_add_element() -> 
-    ?FORALL({List, {Key, Value}},
-        {list({integer(), integer()}), {integer(), integer()}},
-        ?IMPLIES(
-            begin
-                Hashmap = hashmap_init_from_list(List), 
-                hashmap_get_value(Hashmap, Key) == false
-            end,
-            begin
-                Hashmap = hashmap_init_from_list(List), 
-                NewHashmap = hashmap_add(Hashmap, Key, Value),
-                check_hashmap_addition(Hashmap, NewHashmap, Key)
-            end
-        )
-    ).
+prop_add_element() ->
+  ?FORALL(
+    {List, {Key, Value}},
+    {list({integer(), integer()}), {integer(), integer()}},
+    ?IMPLIES(
+      begin Hashmap = hashmap_init_from_list(List), hashmap_get_value(Hashmap, Key) == false end,
+      begin
+        Hashmap = hashmap_init_from_list(List),
+        NewHashmap = hashmap_add(Hashmap, Key, Value),
+        check_hashmap_addition(Hashmap, NewHashmap, Key)
+      end
+    )
+  ).
 
 check_hashmap_removal(OldHashmap, NewHashmap, Key, OldElement) ->
-    case OldElement of 
-        false -> 
-            (OldHashmap#hashmap.elements_size == NewHashmap#hashmap.elements_size)
-            and (hashmap_get_value(NewHashmap, Key) == false);
-        _ ->
-            (OldHashmap#hashmap.elements_size - 1 == NewHashmap#hashmap.elements_size)
-            and (hashmap_get_value(NewHashmap, Key) == false)
-    end.
+  case OldElement of
+    false ->
+      (OldHashmap#hashmap.elements_size == NewHashmap#hashmap.elements_size)
+      and
+      (hashmap_get_value(NewHashmap, Key) == false);
 
-prop_remove_element() -> 
-    ?FORALL({List, {Key, _}},
-        {list({integer(), integer()}), {integer(), integer()}},
-        begin
-            Hashmap = hashmap_init_from_list(List),
-            OldElementValue = hashmap_get_value(Hashmap, Key),
-            NewHashmap = hashmap_remove(Hashmap, Key),
-            check_hashmap_removal(Hashmap, NewHashmap, Key, OldElementValue)
-        end
-    ).
+    _ ->
+      (OldHashmap#hashmap.elements_size - 1 == NewHashmap#hashmap.elements_size)
+      and
+      (hashmap_get_value(NewHashmap, Key) == false)
+  end.
 
-prop_merge_neutral_element() -> 
-    ?FORALL({List},
-        {list({integer(), integer()})},
-            begin
-                Empty = hashmap_init(),
-                Hashmap = hashmap_init_from_list(List), 
-                MergedLeftHashmap = hashmap_merge(Hashmap, Empty),
-                MergedRightHashmap = hashmap_merge(Empty, Hashmap),
-                hashmap_is_equal(MergedLeftHashmap, MergedRightHashmap)
-            end
-        ).
 
-test_properties(_) ->
-    ?assert(get_property_test_result(prop_add_element())),
-    ?assert(get_property_test_result(prop_remove_element())),
-    ?assert(get_property_test_result(prop_merge_neutral_element())).
+prop_remove_element() ->
+  ?FORALL(
+    {List, {Key, _}},
+    {list({integer(), integer()}), {integer(), integer()}},
+    begin
+      Hashmap = hashmap_init_from_list(List),
+      OldElementValue = hashmap_get_value(Hashmap, Key),
+      NewHashmap = hashmap_remove(Hashmap, Key),
+      check_hashmap_removal(Hashmap, NewHashmap, Key, OldElementValue)
+    end
+  ).
+
+prop_merge_neutral_element() ->
+  ?FORALL(
+    {List},
+    {list({integer(), integer()})},
+    begin
+      Empty = hashmap_init(),
+      Hashmap = hashmap_init_from_list(List),
+      MergedLeftHashmap = hashmap_merge(Hashmap, Empty),
+      MergedRightHashmap = hashmap_merge(Empty, Hashmap),
+      hashmap_is_equal(MergedLeftHashmap, MergedRightHashmap)
+    end
+  ).
+
+prop_merge_associativity_element() ->
+  ?FORALL(
+    {FirstList, SecondList, ThirdList},
+    {list({integer(), integer()}), list({integer(), integer()}), list({integer(), integer()})},
+    begin
+      FirstHashmap = hashmap_init_from_list(FirstList),
+      SecondHashmap = hashmap_init_from_list(SecondList),
+      ThirdHashmap = hashmap_init_from_list(ThirdList),
+      MergedLeftHashmap = hashmap_merge(hashmap_merge(FirstHashmap, SecondHashmap), ThirdHashmap),
+      MergedRightHashmap = hashmap_merge(FirstHashmap, hashmap_merge(SecondHashmap, ThirdHashmap)),
+      hashmap_is_equal(MergedLeftHashmap, MergedRightHashmap)
+    end
+  ).
